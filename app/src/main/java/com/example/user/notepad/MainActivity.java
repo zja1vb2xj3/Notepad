5fd34f518bef1b991f10aa9ff8528aeda2502084
PDG <zja1vb2xj3@naver.com>
2017-08-09 01:02:54 +0900

    memoActivityOperate_ButtonClick 메서드 추가

--- app/src/main/java/com/example/user/notepad/MainActivity.java
+++ app/src/main/java/com/example/user/notepad/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.user.notepad;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -12,23 +15,37 @@ public class MainActivity extends Activity {
     private RecyclerViewAdapter adapter;
     private DBHelper dbHelper;
     private final String CLASS_NAME = "MainActivity";
+    private Button memoActivityOperate_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper = new DBHelper(this);
 
-////        dbHelper.insertDataTableIndex("ㅎㅇ");
-////        String result = dbHelper.selectDataTableIndex();
-////        Log.i(CLASS_NAME, result);
-////
-////        dbHelper.deleteDataTableIndex("1");
-////        result = dbHelper.selectDataTableIndex();
-////        Log.i(CLASS_NAME, result);
+        memoActivityOperate_Button = (Button)findViewById(R.id.memoActivityOperate_Button);
+        memoActivityOperate_Button.setOnClickListener(this::memoActivityOperate_ButtonClick);
 
-        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new RecyclerViewAdapter(this);
+
+
+
+//        dbHelper = new DBHelper(this);
+//
+//        dbHelper.insertDataTableIndex("ㅎㅇ");
+//        String result = dbHelper.selectDataTableIndex();
+//        Log.i(CLASS_NAME, result);
+//
+//        dbHelper.deleteDataTableIndex("1");
+//        result = dbHelper.selectDataTableIndex();
+//        Log.i(CLASS_NAME, result);
+//
+//        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        adapter = new RecyclerViewAdapter(this);
+    }
+
+    private void memoActivityOperate_ButtonClick(View view) {
+        Intent intent = new Intent(this, NotepadActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);
     }
 }
