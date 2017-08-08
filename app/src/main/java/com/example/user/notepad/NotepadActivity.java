d8cf40173641f50e61c926e146cf23898b4713f0
PDG <zja1vb2xj3@naver.com>
2017-08-09 00:41:27 +0900

    NotepadActivity 메모등록 버튼, 노트 EditText 추가

--- app/src/main/java/com/example/user/notepad/NotepadActivity.java
+++ app/src/main/java/com/example/user/notepad/NotepadActivity.java
@@ -2,12 +2,18 @@ package com.example.user.notepad;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NotepadActivity extends Activity {
+    private Button noteRegister_Button;
+    private EditText note_EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notepad);
+        noteRegister_Button = (Button)findViewById(R.id.noteRegister_Button);
+        note_EditText = (EditText)findViewById(R.id.note_EditText);
     }
 }
