78ee1110aea0057a262cda33be56cf7278a4a43f
PDG <zja1vb2xj3@naver.com>
2017-09-10 01:51:02 +0900

    Package 정리

--- app/src/main/java/com/example/user/notepad/Activity/MainActivity.java
+++ app/src/main/java/com/example/user/notepad/Activity/MainActivity.java
@@ -166,8 +166,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
-
-
         return super.onCreateOptionsMenu(menu);
     }
 
